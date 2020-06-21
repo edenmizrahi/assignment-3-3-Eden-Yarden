5867ddbab7c348082097ed44362861e10ab86c7f
yarden <yardensch95@gmail.com>
2020-06-21 09:49:41 +0300

    ad

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="container">
-    <h1 class="title">Search Page</h1>
+    <h1 class="title">{{ header }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      header: "Search Page",
+    };
+  },
+};
+</script>
+
+<style></style>
